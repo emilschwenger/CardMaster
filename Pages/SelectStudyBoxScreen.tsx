58a57bdc1cef372e431948336caa1c5da1397b90
Emil Schwenger <emil.schwenger@tum.de>
2022-11-23 20:03:52 +0100

    UI - study box selection

---
+++ Pages/SelectStudyBoxScreen.tsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { Input, Button, ListItem } from '@rneui/themed';
+import * as SQLiteHelper from '../Utils/SQLiteHelper';
+import { CMHeader } from "../Components/CMHeader";
+import * as SQLite from 'expo-sqlite';
+
+
+interface SelcetStudyBoxProps {
+    navigation: any
+}
+
+interface SelectStudyBoxState {
+    boxList: []
+}
+
+export function SelectStudyBox (props: SelcetStudyBoxProps) {
+
+    const [boxList, setBoxList] = useState([]);
+
+    useEffect(() => {
+        SQLiteHelper.readAllBox((results: SQLite.SQLResultSet) => {
+            if(JSON.stringify(boxList)==JSON.stringify(results.rows._array)) {
+                return;
+            }
+            setBoxList(results.rows._array);
+        });
+    });
+
+    const studyBox = (box: SQLiteHelper.box) => {
+        props.navigation.navigate('StudyCards', {box});
+    }
+
+    return (
+        <View>
+            <CMHeader title="CardMaster"/>
+            <View>
+                <View style={{height: 5}}/>
+                <Button title={'Back'} onPress={()=>{
+                    console.log(props);
+                    props.navigation.navigate('Home');
+                }}/>
+                <View style={{height: 5}}/>
+            </View>
+            <ScrollView>
+                {
+                    boxList.map((l: SQLiteHelper.box, i)=>(
+                        <ListItem key={i}>
+                            <ListItem.Content style={style.listItemStyle}>
+                                    <ListItem.Title style={style.textStyle}>{l.name}</ListItem.Title>
+                                    <View style={style.buttonContainer}>
+                                        <Button title={'Study'} onPress={() => studyBox(l)}/>
+                                    </View>
+                            </ListItem.Content>
+                        </ListItem>
+                    ))
+                }
+            </ScrollView>
+        </View>
+    );
+}
+
+const style = StyleSheet.create({
+    listItemStyle: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    textStyle: {
+        fontFamily: 'sans-serif',
+        fontWeight: 'bold',
+        color: 'black',
+    },
+    buttonContainer: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: 65
+    }
+});
